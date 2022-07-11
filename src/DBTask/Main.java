a3a4be004c5cabe1aaf05d341189f21959a1fa30
Stonewall1 <impression97p@gmail.com>
2022-07-11 22:14:20 +0300

    List of students and full info about students added

---
+++ src/DBTask/Main.java
@@ -0,0 +1,9 @@
+package DBTask;
+
+public class Main {
+    public static void main(String[] args) {
+        WorkingWithDatabase data = new WorkingWithDatabase();
+        data.getFullDBInfo();
+        data.getListOfStudents();
+    }
+}
---
+++ src/DBTask/WorkingWithDatabase.java
@@ -0,0 +1,48 @@
+package DBTask;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WorkingWithDatabase {
+    public void getFullDBInfo() {
+        try (Connection connection = DataBaseConnection.getConnection()) {
+
+            HashMap<String, String> studentsInfo = new HashMap<>();
+
+            Statement statement = connection.createStatement();
+            ResultSet studentsFullInfo = statement.executeQuery("SELECT * FROM students");
+            while (studentsFullInfo.next()) {
+                studentsInfo.put(studentsFullInfo.getString("name"),
+                                 studentsFullInfo.getString("country"));
+            }
+            for (Map.Entry<String, String> entry : studentsInfo.entrySet()) {
+                System.out.println("Name : " + entry.getKey() + "\n" + "Country : " + entry.getValue() + "\n-----------");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getListOfStudents() {
+        List<String> listOfStudents = new ArrayList<>();
+        try (Connection connection = DataBaseConnection.getConnection()) {
+
+            Statement statement = connection.createStatement();
+            ResultSet studentsFullInfo = statement.executeQuery("SELECT * FROM students");
+            while (studentsFullInfo.next()) {
+                listOfStudents.add(studentsFullInfo.getString("name"));
+            }
+            System.out.println("---All students list---");
+            for(String name : listOfStudents){
+                System.out.println(name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
